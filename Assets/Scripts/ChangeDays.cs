32745c4b5d5dae078f5b06575213687236b77672
Alex Kim <soulside44@gmail.com>
2019-11-20 17:12:22 +0900

    Rotation
    지구 자전, 달 공전 구현

---
+++ Assets/Scripts/ChangeDays.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDays : MonoBehaviour
+{
+private void Start() {
+    
+}
+private void Update() {
+    float cal = Mathf.PingPong(Time.time, 1);
+
+}
+}
---
+++ Assets/Scripts/Moon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.down,speed*Time.deltaTime );
+        Vector3 relativePos = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(relativePos);
+    }
+}
